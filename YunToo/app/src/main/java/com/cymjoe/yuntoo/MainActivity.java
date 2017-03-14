76ba54662780b7838925372bc26b526640d9ca15
cymjoe <cymjoe@126.com>
2017-03-14 17:04:42 +0800

    测试

---
+++ YunToo/app/src/main/java/com/cymjoe/yuntoo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cymjoe.yuntoo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        for (int i = 0; i < 30; i++) {
+
+        }
+    }
+}
